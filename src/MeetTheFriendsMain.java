e38541080a5e742eac1dcdc33cc1838fcbfcbaa4
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-30 15:12:33 +0900

    GUI layout

--- src/MeetTheFriendsMain.java
+++ src/MeetTheFriendsMain.java
@@ -1,9 +1,21 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.sql.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class MeetTheFriendsMain extends JFrame {
     JPanel pane, paneCenter;
+    JPanel topPanel, bottomPanel;
+    JPanel column1, column2, column3;
+    JPanel selAnimalPanel, selPlacePanel;
+
+    JComboBox<String> mediaBox, episodeBox;
+    JList<String> animalList, placeList;
+    DefaultListModel<String> animalListModel, placeListModel;
+    JButton connectBtn;
+    JLabel selAnimalLabel, selPlaceLabel;
+    JLabel statusLabel;
 
     String driverClassName = "org.postgresql.Driver";
     String url = "jdbc:postgresql://localhost/test";
@@ -25,9 +37,78 @@ public class MeetTheFriendsMain extends JFrame {
         initializeGUI();
     }
 
+    class ConnectAction extends AbstractAction {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
     private void initializeGUI() {
         pane = (JPanel)getContentPane();
         pane.setLayout(new BorderLayout());
+        paneCenter = new JPanel();
+        paneCenter.setLayout(new BoxLayout(paneCenter, BoxLayout.X_AXIS));
+        //paneCenter.setLayout(new FlowLayout(FlowLayout.LEFT));
 
+
+        //一番上の部分
+        topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 4));
+        Action connectAction = new ConnectAction();
+        connectAction.putValue(Action.NAME, "データベースに接続");
+        connectBtn = new JButton(connectAction);
+        topPanel.add(connectBtn);
+        pane.add(topPanel, BorderLayout.PAGE_START);
+
+        //一番下の部分（ステータスバー）
+        bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 4));
+        statusLabel = new JLabel("ステータスバー");
+        bottomPanel.add(statusLabel);
+        pane.add(bottomPanel, BorderLayout.PAGE_END);
+
+        //メインの内容
+        //カラム1
+        column1 = new JPanel();
+        column1.setLayout(new BoxLayout(column1, BoxLayout.Y_AXIS));
+
+        mediaBox = new JComboBox<>();
+        column1.add(mediaBox);
+        episodeBox = new JComboBox<>();
+        column1.add(episodeBox);
+        animalListModel = new DefaultListModel<>();
+        animalList = new JList<>(animalListModel);
+        JScrollPane animalScrollPane = new JScrollPane(animalList);
+        column1.add(animalScrollPane);
+        paneCenter.add(column1);
+
+        //カラム2
+        column2 = new JPanel();
+        column2.setLayout(new BoxLayout(column2, BoxLayout.Y_AXIS));
+
+        placeListModel = new DefaultListModel<>();
+        placeList = new JList<>(animalListModel);
+        JScrollPane placeScrollPane = new JScrollPane(placeList);
+        column2.add(placeScrollPane);
+        paneCenter.add(column2);
+
+        //カラム3
+        column3 = new JPanel();
+        column3.setLayout(new BoxLayout(column3, BoxLayout.Y_AXIS));
+
+        selPlacePanel = new JPanel();
+        selPlacePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        selPlaceLabel = new JLabel("選択された場所");
+        selPlacePanel.add(selPlaceLabel);
+        column3.add(selPlacePanel);
+        selAnimalPanel = new JPanel();
+        selAnimalPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        selAnimalLabel = new JLabel("選択された動物");
+        selAnimalPanel.add(selAnimalLabel);
+        column3.add(selAnimalPanel);
+        paneCenter.add(column3);
+
+        pane.add(paneCenter);
     }
 }
