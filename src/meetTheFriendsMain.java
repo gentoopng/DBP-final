98d6fffb473fa1d4fdb55b96c393d7d0262c16c2
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-30 13:49:20 +0900

    Initialize GUI

--- src/meetTheFriendsMain.java
+++ src/meetTheFriendsMain.java
@@ -1,3 +1,33 @@
-public class meetTheFriendsMain {
+import java.awt.*;
+import java.sql.*;
+import javax.swing.*;
 
+public class meetTheFriendsMain extends JFrame {
+    JPanel pane, paneCenter;
+
+    String driverClassName = "org.postgresql.Driver";
+    String url = "jdbc:postgresql://localhost/test";
+    String user = "dbpuser";
+    String password = "hogehoge";
+    Statement statement;
+    Connection connection;
+    ResultSet resultSet;
+
+    public static void main(String[] args) {
+        JFrame w = new meetTheFriendsMain("Meet the Friends");
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setSize(800, 400);
+        w.setVisible(true);
+    }
+
+    public meetTheFriendsMain(String title) {
+        super(title);
+        initializeGUI();
+    }
+
+    private void initializeGUI() {
+        pane = (JPanel)getContentPane();
+        pane.setLayout(new BorderLayout());
+
+    }
 }
